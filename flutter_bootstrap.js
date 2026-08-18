a135f0d4226cd884f270e6a0225d18d5a05ced6f
ulischlegelmilch-hue <uli.schlegelmilch@googlemail.com>
2026-08-18 14:28:06 +0200

    Update: Supabase-Anbindung

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4190723917" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2389108230" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
