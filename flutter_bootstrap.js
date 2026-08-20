d335b5bc1cfe2b36dfd7cb54ec65bbf1f524e608
ulischlegelmilch-hue <uli.schlegelmilch@googlemail.com>
2026-08-20 14:33:41 +0200

    Update: Favicon/Touch-Icon umbenannt (Cache-Bust)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2887681077" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1535365312" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
